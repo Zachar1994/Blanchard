11d7a2caa349f807e3cf8ad4a45af9b804da1d0c
Zackhar <zahar_medovshikov@mail.ru>
2021-10-31 12:32:33 +0300

    almost done

--- scrollbar.js
+++ scrollbar.js
@@ -1,6 +1,11 @@
 new SimpleBar(document.querySelector(".simplebar"), {
+
     /* чтобы изначально ползунок был виден */
     autoHide: false,
+
     /* с помощью этого значения вы можете управлять высотой ползунка*/
-    scrollbarMaxSize: 28,
+    scrollbarMaxSize: 10,
   });
+
+  
+  
